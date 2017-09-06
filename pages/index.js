cba1b290f44c05989de8dcff196fcef8b0f1eaee
Pablo Varela <pablovarela182@gmail.com>
2017-09-06 15:36:49 +0200

    Client fetch (fix #4)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FadeIn from 'react-fade-in'
+import {CubeGrid} from 'better-react-spinkit'
 import getMatches from 'livesoccertv-parser'
 import Layout from '../components/layout'
 import Match from '../components/match'
@@ -9,19 +10,38 @@ const filterPlayed = m => !m.played
 const translateMatches = ms => ms.map(m => Object.assign(m, {date: translate.date(m.date)}))
 
 export default class Index extends React.Component {
-  static async getInitialProps () {
+  constructor (props) {
+    super(props)
+    this.state = { matches: [] }
+  }
+
+  async componentDidMount () {
     let matches = await getMatches('spain', 'real-madrid')
     matches = matches.filter(filterPlayed)
     matches = translateMatches(matches)
-    return { matches }
+    return this.setState({ matches })
   }
 
   render () {
+    if (!this.state.matches.length) {
+      return (
+        <Layout>
+          <div>
+            <CubeGrid size={50} color='royalblue' />
+          </div>
+          <style jsx>{`
+            div {
+              margin: 3em;
+            }
+          `}</style>
+        </Layout>
+      )
+    }
     return (
       <Layout>
         <FadeIn>
           {
-            this.props.matches.map((m,i) => <Match key={i} match={m} />)
+            this.state.matches.map((m,i) => <Match key={i} match={m} />)
           }
         </FadeIn>
       </Layout>
