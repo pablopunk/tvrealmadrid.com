e0009b66d259f09bb918c4f3d3b4c85ba9fc3878
Pablo Varela <pablovarela182@gmail.com>
2018-01-21 17:01:28 +0100

    Add spinkit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FadeIn from 'react-fade-in'
 import fetch from 'isomorphic-fetch'
+import {DoubleBounce} from 'better-react-spinkit'
 import Layout from '../components/layout'
 import Match from '../components/match'
 import translate from '../util/translate'
@@ -29,7 +30,9 @@ export default class Index extends React.Component {
     if (this.state.matches.length === 0) {
       return (
         <Layout>
-          <div style={{marginTop: '20px'}}>Loading...</div>
+          <div style={{marginTop: '20px'}}>
+            <DoubleBounce size={50} />
+          </div>
         </Layout>
       )
     }
